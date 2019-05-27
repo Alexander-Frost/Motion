ca435c0e4ea52618d8ac58e5f3ba25a4d3529f35
Alex <alexcc17@gmail.com>
2019-05-27 15:17:31 -0400

    Core Motion experiment

--- Motion Experiments/ViewController.swift
+++ Motion Experiments/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let manager = CMMotionActivityManager()
+        manager.startActivityUpdates(to: .main) { (activity) in
+            guard let activity = activity else {
+                return
+            }
+            
+            var modes: Set<String> = []
+            if activity.walking {
+                modes.insert("🚶‍")
+            }
+            
+            if activity.running {
+                modes.insert("🏃‍")
+            }
+            
+            if activity.cycling {
+                modes.insert("🚴‍")
+            }
+            
+            if activity.automotive {
+                modes.insert("🚗")
+            }
+            
+            print(modes.joined(separator: ", "))
+        }
     }
 
 
